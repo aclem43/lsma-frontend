716a965fc565069f44b004b3cc2c9f26d8f4dff6
aclem43 <aclemeur@gmail.com>
2022-06-11 17:15:50 +1000

    Project Setup + Basic Design And Functions

---
+++ src/components/ServiceList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="service">
+    <table>
+      <tr class="tablehead">
+        <th class="tableborder">Service Display Name</th>
+        <th class="tableborder">Service Name</th>
+        <th class="tableborder">Status</th>
+        <th>View</th>
+      </tr>
+      <div>
+        <tr id="" class="tablerow">
+          <td class="tableborder">Service Name</td>
+          <td class="tableborder">Service</td>
+          <td class="tableborder">Running</td>
+          <td><button>View</button></td>
+        </tr>
+      </div>
+    </table>
+  </div>
+</template>
---
+++ src/components/ServiceManager.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { getServices } from "../js/Data.ts";
+
+getServices();
+</script>
+
+<template>
+  <div class="servicemanager">
+    <h4>Current Service: {name}</h4>
+    <div class="flex">
+      <table class="tablealign">
+        <tr>
+          <td>Status:</td>
+          <td>Running</td>
+        </tr>
+        <tr>
+          <td>Display Name:</td>
+          <td>{name}</td>
+        </tr>
+      </table>
+      <div class="controller">
+        <div>
+          <button>Start</button>
+        </div>
+        <div>
+          <button>Restart</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
